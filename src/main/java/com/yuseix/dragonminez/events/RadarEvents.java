03dbfc9c523024160ca9bdbbbc98dc662a113ed9
Bruneitor123 <brunitoaas@gmail.com>
2025-02-04 14:03:12 -0800

    Flechita god ayayay

--- src/main/java/com/yuseix/dragonminez/events/RadarEvents.java
+++ src/main/java/com/yuseix/dragonminez/events/RadarEvents.java
@@ -6,8 +6,6 @@ import com.yuseix.dragonminez.init.items.custom.DragonBallRadarItem;
 import com.yuseix.dragonminez.init.items.custom.NamekDragonBallRadarItem;
 import com.yuseix.dragonminez.network.C2S.DragonRadarC2S;
 import com.yuseix.dragonminez.network.ModMessages;
-import com.yuseix.dragonminez.world.DragonBallGenProvider;
-import com.yuseix.dragonminez.world.NamekDragonBallGenProvider;
 import com.yuseix.dragonminez.worldgen.dimension.ModDimensions;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
@@ -22,7 +20,6 @@ import net.minecraftforge.client.event.RenderGuiOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.joml.Quaternionf;
-import org.joml.Vector3f;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -82,7 +79,7 @@ public class RadarEvents {
 
 				// Renderizar posiciones si ya están actualizadas
 				if (isOverworld) renderDballRadar(gui, player, radarRange, closestDballPositions, centerX, centerY);
-				else if (isNamek) renderDballRadar(gui, player, radarRange, closestNDballPositions, centerX, centerY);
+				else renderDballRadar(gui, player, radarRange, closestNDballPositions, centerX, centerY);
 			} else if ((isOverworld && offhandRadarItem.getItem() instanceof DragonBallRadarItem) ||
 					(isNamek && offhandRadarItem.getItem() instanceof NamekDragonBallRadarItem)) {
 				// Get radar range from NBT or use default
@@ -92,9 +89,9 @@ public class RadarEvents {
 				// Draw radar background
 				int centerX = 0;
 				if (mc.getWindow().getWidth() < 1080) {
-					centerX = (mc.getWindow().getGuiScaledWidth() /2) - radarSize - 60;
+					centerX = (mc.getWindow().getGuiScaledWidth() / 2) - radarSize - 60;
 				} else {
-					centerX = (mc.getWindow().getGuiScaledWidth() /2) - radarSize - 310;
+					centerX = (mc.getWindow().getGuiScaledWidth() / 2) - radarSize - 310;
 				}
 				int centerY = mc.getWindow().getGuiScaledHeight() - radarSize - 10;
 
@@ -113,7 +110,7 @@ public class RadarEvents {
 
 				// Renderizar posiciones si ya están actualizadas
 				if (isOverworld) renderDballRadar(gui, player, radarRange, closestDballPositions, centerX, centerY);
-				else if (isNamek) renderDballRadar(gui, player, radarRange, closestNDballPositions, centerX, centerY);
+				else renderDballRadar(gui, player, radarRange, closestNDballPositions, centerX, centerY);
 			}
 		}
 	}
@@ -142,21 +139,37 @@ public class RadarEvents {
 				// Draw yellow dot
 				gui.blit(fondo, radarX - 2, radarY - 2, 130, 0, 6, 6);
 			} else {
+
 				// Draw yellow arrow
 				int arrowX = (int) (centerX + 61 - 50 * Math.cos(adjustedAngle));
 				int arrowY = (int) (centerY + 87 - 50 * Math.sin(adjustedAngle));
-				float arrowRotation = (float) (-adjustedAngle + Math.PI/2);
 
+				// Compute vector from radar center to arrow position
+				int radarCenterX = centerX + 61;
+				int radarCenterY = centerY + 87;
+				double screenDx = arrowX - radarCenterX;
+				float arrowRotation = getArrowRotation(arrowY, radarCenterY, screenDx);
 
+				// Now proceed to render with arrowRotation
 				RenderSystem.setShaderTexture(0, fondo);
 				gui.pose().pushPose();
 				gui.pose().translate(arrowX, arrowY, 0);
 				gui.pose().mulPose(new Quaternionf().rotationZ(arrowRotation));
 				gui.pose().translate(-3.5f, -3.0f, 0);
-				gui.blit(fondo, 0, + 2, 130, 8, 7, 6);
+				gui.blit(fondo, 0, +2, 130, 8, 7, 6);
 				gui.pose().popPose();
+
 			}
 		}
+	}
+
+	private static float getArrowRotation(int arrowY, int radarCenterY, double screenDx) {
+		double screenDy = arrowY - radarCenterY;
+
+		// Determine the angle of that vector (atan2 gives angle relative to positive X)
+		double screenAngle = Math.atan2(screenDy, screenDx);
+
+		return (float) (screenAngle + Math.PI / 2);
 	}
 
 	public static void updateDragonBallsPositions(List<BlockPos> positionsDball) {
