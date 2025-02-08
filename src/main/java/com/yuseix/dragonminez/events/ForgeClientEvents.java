37ca3098d2b432914386bd169ee093305f4c9406
Shokkoh <ezshokkoh@gmail.com>
2025-02-08 19:47:14 -0300

    Fix LocaPlayer in ForgeEvents crash

--- src/main/java/com/yuseix/dragonminez/events/ForgeClientEvents.java
+++ src/main/java/com/yuseix/dragonminez/events/ForgeClientEvents.java
@@ -6,11 +6,18 @@ import com.yuseix.dragonminez.network.ModMessages;
 import com.yuseix.dragonminez.stats.DMZStatsCapabilities;
 import com.yuseix.dragonminez.stats.DMZStatsProvider;
 import com.yuseix.dragonminez.utils.Keys;
+import com.yuseix.dragonminez.world.DragonBallGenProvider;
+import com.yuseix.dragonminez.world.NamekDragonBallGenProvider;
+import com.yuseix.dragonminez.worldgen.dimension.ModDimensions;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.client.event.RenderGuiOverlayEvent;
 import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -21,6 +28,26 @@ public class ForgeClientEvents {
 	public static void onKeyInput(InputEvent.Key event) {
 		if (Keys.STATS_MENU.consumeClick()) {
 			ModMessages.sendToServer(new MenuC2S());
+		}
+	}
+
+	@SubscribeEvent
+	public static void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
+		LocalPlayer player = (LocalPlayer) event.getEntity();
+
+		if (event.getEntity().level() instanceof ServerLevel serverLevel) {
+			if (serverLevel.dimension() == Level.OVERWORLD) {
+				serverLevel.getCapability(DragonBallGenProvider.CAPABILITY).ifPresent(cap -> {
+					cap.loadFromSavedData(serverLevel);
+					RadarEvents.updateDragonBallsPositions(cap.dragonBallPositions);
+				});
+			}
+			if (serverLevel.dimension() == ModDimensions.NAMEK_DIM_LEVEL_KEY) {
+				serverLevel.getCapability(NamekDragonBallGenProvider.CAPABILITY).ifPresent(cap -> {
+					cap.loadFromSavedData(serverLevel);
+					RadarEvents.updateNamekDragonBallsPositions(cap.namekDragonBallPositions);
+				});
+			}
 		}
 	}
 
